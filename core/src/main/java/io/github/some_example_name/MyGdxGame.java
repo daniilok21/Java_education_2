8f5f5928af4ae21613d47f8f1be65b24a2620868
daniilok21 <sergeevdaniil383@gmail.com>
2025-09-30 17:21:37 +0300

    2

---
+++ core/src/main/java/io/github/some_example_name/MyGdxGame.java
@@ -0,0 +1,31 @@
+package io.github.some_example_name;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MyGdxGame extends Game {
+
+    public SpriteBatch batch;
+    public OrthographicCamera camera;
+
+    public final int SCR_WIDTH = 1280, SCR_HEIGHT = 720;
+
+    ScreenGame screenGame;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, SCR_WIDTH, SCR_HEIGHT);
+
+        screenGame = new ScreenGame(this);
+        setScreen(screenGame);
+    }
+
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+    }
+}
